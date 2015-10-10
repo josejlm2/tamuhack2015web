10a08dd83772ce126acc65bf924d3dc11e628617
Jose Manriquez <josejlm2@hotmail.com>
2015-10-10 06:19:45 -0500

    custom template finished

---
+++ pages/js.php
@@ -0,0 +1,5 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/animate.js"></script>
