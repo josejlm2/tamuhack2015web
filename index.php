9f569f39233910491e2a00d022728f0f47d63364
Jose Manriquez <josejlm2@hotmail.com>
2015-10-10 05:22:43 -0500

    new layout update

--- index.php
+++ index.php
@@ -1,16 +1,163 @@
+<?php
+// Server in the this format: <computer>\<instance name> or 
+// <server>,<port> when using a non default port number
+$server = 'tcp:ozfevv4o6f.database.windows.net,1433';
+
+// Connect to MSSQL
+$link = mssql_connect($server, 'josejlm2', 'TamuHack2015');
+
+if (!$link) {
+    die('Something went wrong while connecting to MSSQL');
+}
+?>
+
+
+
+
 <!DOCTYPE html>
-<html>
-<head>
-<title>GoodTimes2</title>
-<meta charset="UTF-8">
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>GoodTimes</title>
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="css/styles.css" rel="stylesheet">
+		<link href="css/animate.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/animate.css">
+		<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</head>
-<body>
-</body>
+<header class="navbar navbar-default navbar-static-top" id="nav-bar" role="banner">
+  <div class="container">
+		<a href="/" id="brand" class="navbar-brand">GoodTimes</a>
+  </div>
+</header>
+
+<!-- Begin Body -->
+<div class="container">
+	<div class="row">
+  			<div class="col-md-2" id="leftCol" >
+              	
+				<div class="well wow slideInDown" > 
+              	<ul class="nav nav-stacked" id="sidebar">
+                  <li id="profile"><a href="#sec1"><i class="fa fa-user"></i>   Profile</a></li>
+                  <li id="myevents"><a href="#sec2"><i class="fa fa-calendar"></i>   My Events</a></li>
+                  <li id="invites"><a href="#sec3"><i class="fa fa-envelope-o"></i>    Invites</a></li>
+                  <li id="friends"><a href="#sec4"><i class="fa fa-users"></i>    Friends</a></li>
+              	</ul>
+  				</div>
+
+      		</div>  
+      		<div class="col-md-10">
+              	<h2 id="sec0">Content</h2>
+                At Bootply we like to build simple Bootstrap templates that utilize the code Bootstap CSS without a lot of customization. Sure you can 
+              	find a lot of Bootstrap themes and inspiration, but these templates tend to be heavy on customization.
+              	
+            
+           
+              	<hr class="col-md-12">
+              	Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut.              
+              	Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!
+              	
+              
+              
+              	<h2 id="sec1">Content</h2>
+              	<p>
+                Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut.
+              	</p>
+              	<div class="row">
+                  <div class="col-md-6">
+                    <div class="panel panel-default">
+                      <div class="panel-heading"><h3>Hello.</h3></div>
+                      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+                      Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+                      dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+                      Aliquam in felis sit amet augue.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                      <div class="panel panel-default">
+                      <div class="panel-heading"><h3>Hello.</h3></div>
+                      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+                      Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+                      dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+                      Aliquam in felis sit amet augue.
+                      </div>
+                    </div>
+                  </div>  
+              	</div>
+              
+              	<hr>
+              
+              	<h2 id="sec2">Section 2</h2>
+              	<p>
+                Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!
+              	</p>
+              	<div class="row">
+              		<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
+                  	<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
+                  	<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
+              	</div>
+              
+              	<hr>
+              
+              	<h2 id="sec3">Section 3</h2>
+      			Images are responsive sed @mdo but sum are more fun peratis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut..
+              	<br>
+                Fos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut..
+              
+              
+              	<h2 id="sec4">Section 4</h2>
+      			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+              
+              
+              	<hr>
+              <h4><a href="http://bootply.com/100993">Edit on Bootply</a></h4>
+              	<hr>
+              	
+              	
+      		</div> 
+  	</div>
+</div>
+	<!-- script references -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/scripts.js"></script>
+		<script src="js/wow.min.js"></script>
+		<script src="js/animate.js"></script>
+	</body>
+</html>
---
+++ js/animate.js
@@ -0,0 +1,44 @@
+
+$( '#brand' ).hover(
+  function() {
+	$( this ).addClass( 'animated pulse' );
+  }, function() {
+	$( this ).removeClass( 'animated pulse' );
+  }
+);
+
+$( '#profile' ).hover(
+  function() {
+	$( this ).addClass( 'animated rubberBand' );
+  }, function() {
+	$( this ).removeClass( 'animated rubberBand' );
+  }
+);
+
+$( '#myevents' ).hover(
+  function() {
+	$( this ).addClass( 'animated rubberBand' );
+  }, function() {
+	$( this ).removeClass( 'animated rubberBand' );
+  }
+);
+
+$( '#invites' ).hover(
+  function() {
+	$( this ).addClass( 'animated rubberBand' );
+  }, function() {
+	$( this ).removeClass( 'animated rubberBand' );
+  }
+);
+
+$( '#friends' ).hover(
+  function() {
+	$( this ).addClass( 'animated rubberBand' );
+  }, function() {
+	$( this ).removeClass( 'animated rubberBand' );
+  }
+);
+
+
+          
+ new WOW().init();
