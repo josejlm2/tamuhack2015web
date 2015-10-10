bb19b341d1f3b54a08c30e123fbf7a5fb51bd6b2
Mona <ohnarya@gmail.com>
2015-10-09 23:46:13 -0500

    test log out

--- src/login.php
+++ src/login.php
@@ -80,8 +80,8 @@
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-      // document.getElementById('status').innerHTML =
-      //   'Thanks for logging in, ' + response.name + '!';
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
 
       //document.location = '/src/profile.php';
     });
