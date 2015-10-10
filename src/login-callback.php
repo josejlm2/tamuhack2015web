a996bd5fcbb3f235b9a8692384b093226898bf1d
Mona <ohnarya@gmail.com>
2015-10-10 03:03:48 -0500

    login

--- src/login-callback.php
+++ src/login-callback.php
@@ -1,5 +1,11 @@
 <?php
-$fb = new Facebook\Facebook([/* . . . */]);
+require_once __DIR__ . '/vendor/autoload.php';
+session_start();
+$fb = new Facebook\Facebook([
+  'app_id' => '1497137333945789',
+  'app_secret' => '18aec6b9e0c32ab5703e9cae736685c1',
+  'default_graph_version' => 'v2.5',
+  ]);
 
 $helper = $fb->getRedirectLoginHelper();
 try {
@@ -17,7 +23,7 @@ try {
 if (isset($accessToken)) {
   // Logged in!
   $_SESSION['facebook_access_token'] = (string) $accessToken;
-
+  header( 'Location: http://localhost:8888/src/profile.php' ) ;
   // Now you can redirect to another page and use the
   // access token from $_SESSION['facebook_access_token']
 }
