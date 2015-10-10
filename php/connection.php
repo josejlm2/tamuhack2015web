3f79717ba2c4797676e97309762d5cef9977f013
Jose Manriquez <josejlm2@hotmail.com>
2015-10-10 08:06:57 -0500

    test db connection

--- php/connection.php
+++ php/connection.php
@@ -2,4 +2,11 @@
 $connectionInfo = array("UID" => "josejlm2@ozfevv4o6f", "pwd" => "TamuHack2015", "Database" => "tamuhack2015_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:ozfevv4o6f.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if (empty($con)){
+	
+	echo "error ! Nothing here";
+	die();
+	
+}
 ?>
