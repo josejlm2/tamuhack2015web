92654dfcc1ec42689ba8779fc9aa8fb9aa67af5c
Jose Manriquez <josejlm2@hotmail.com>
2015-10-10 04:27:32 -0500

    initial template done

---
+++ test/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>GoodTimes2</title>
+<!-- Mobile support -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Twitter Bootstrap -->
+    <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Material Design for Bootstrap -->
+    <!--<link href="dist/css/roboto.min.css" rel="stylesheet">
+    // <link href="dist/css/material-fullpalette.css" rel="stylesheet">
+    // <link href="dist/css/ripples.min.css" rel="stylesheet">
+	-->
+
+    <!-- Dropdown.js -->
+    <link href="//cdn.rawgit.com/FezVrasta/dropdown.js/master/jquery.dropdown.css" rel="stylesheet">
+
+    <!-- jQuery -->
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+
+</head>
+<body>
+
+
+                                <div class="navbar-header" style="height:30px; !important">
+                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+                                    <a class="navbar-brand" href="javascript:void(0)">Brand<div class="ripple-wrapper"></div></a>
+                                </div>
+                                <div class="navbar-collapse collapse navbar-responsive-collapse" style="background-color: #4A828F; height:30px !important;">
+                                    <ul class="nav navbar-nav">
+                                        <li class="active"><a href="javascript:void(0)">Active<div class="ripple-wrapper"></div></a></li>
+                                        <li><a href="javascript:void(0)">Link<div class="ripple-wrapper"></div></a></li>
+                                        <li class="dropdown">
+                                            <a aria-expanded="false" href="bootstrap-elements.html" data-target="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b><div class="ripple-wrapper"></div></a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="javascript:void(0)">Action</a></li>
+                                                <li><a href="javascript:void(0)">Another action</a></li>
+                                                <li><a href="javascript:void(0)">Something else here</a></li>
+                                                <li class="divider"></li>
+                                                <li class="dropdown-header">Dropdown header</li>
+                                                <li><a href="javascript:void(0)">Separated link</a></li>
+                                                <li><a href="javascript:void(0)">One more separated link</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                    <form class="navbar-form navbar-left">
+                                        <input class="form-control col-lg-8" placeholder="Search" type="text">
+                                    </form>
+                                    <ul class="nav navbar-nav navbar-right">
+                                        <li><a href="javascript:void(0)">Link</a></li>
+                                        <li class="dropdown">
+                                            <a aria-expanded="false" href="bootstrap-elements.html" data-target="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b><div class="ripple-wrapper"></div></a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="javascript:void(0)">Action</a></li>
+                                                <li><a href="javascript:void(0)">Another action</a></li>
+                                                <li><a href="javascript:void(0)">Something else here</a></li>
+                                                <li class="divider"></li>
+                                                <li><a href="javascript:void(0)">Separated link</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            
+</body>
+</html>
